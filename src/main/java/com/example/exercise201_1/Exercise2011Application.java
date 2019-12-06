c7b455eafb968462a715df3982990a0fd3ff949b
Jared Obozele <jared.obozele@gmail.com>
2019-12-06 16:45:13 -0500

    adding new files

---
+++ src/main/java/com/example/exercise201_1/Exercise2011Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise201_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise2011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise2011Application.class, args);
+	}
+
+}
